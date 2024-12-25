50be0a651d5bccb8af72fd590c9b0a8ff3475724
tobiasnunn <59334337+tobiasnunn@users.noreply.github.com>
2024-12-25 17:46:13 +0000

    lotta stuff
    largely this is the half way mark for making the table and doing rando checkm stuff (it would have been wise to do them seperately but i was waiting and figured i should use the time productively). so now there is a directory of flye stuff and some script changes on some documents. still need to make the table, find out just what in the hell the outputs from checkm mean and notebooking.

---
+++ 00_scripts/assemblytabledatagetter.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "beepboop, copying files, boopbeep"
+
+cp /scratch/scw2160/02_outputs/flye_asm/flye_asm_1Dt100g_part2subset/assembly_info.txt ./flye_assembly/1Dt100g_assembly_info.txt
+cp /scratch/scw2160/02_outputs/flye_asm/flye_asm_1Dt100h_part2subset/assembly_info.txt ./flye_assembly/1Dt100h_assembly_info.txt
+cp /scratch/scw2160/02_outputs/flye_asm/flye_asm_1Dt1h_part2/assembly_info.txt ./flye_assembly/1Dt1h_assembly_info.txt
+cp /scratch/scw2160/02_outputs/flye_asm/flye_asm_1Dt2d_part2/assembly_info.txt ./flye_assembly/1Dt2d_assembly_info.txt
+cp /scratch/scw2160/02_outputs/flye_asm/flye_asm_2Dt1e_part2/assembly_info.txt ./flye_assembly/2Dt1e_assembly_info.txt
+cp /scratch/scw2160/02_outputs/flye_asm/flye_asm_2Dt1l_part2/assembly_info.txt ./flye_assembly/2Dt1l_assembly_info.txt
+cp /scratch/scw2160/02_outputs/flye_asm/flye_asm_3Dt1c_part2/assembly_info.txt ./flye_assembly/3Dt1c_assembly_info.txt
+cp /scratch/scw2160/02_outputs/flye_asm/flye_asm_3Dt2c_part2/assembly_info.txt ./flye_assembly/3Dt2c_assembly_info.txt
+cp /scratch/scw2160/02_outputs/flye_asm/flye_asm_3Dt2h_part2/assembly_info.txt ./flye_assembly/3Dt2h_assembly_info.txt
+cp /scratch/scw2160/02_outputs/flye_asm/flye_asm_3Dt2j_part2subset/assembly_info.txt ./flye_assembly/3Dt2j_assembly_info.txt
+echo "beepboop finished boopbeep"
