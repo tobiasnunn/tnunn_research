59dc0c7ebf2349b056e2bb1bab47d62647c6b7b7
tobiasnunn <59334337+tobiasnunn@users.noreply.github.com>
2024-12-28 16:54:26 +0000

    flye.log stuff NOT notebook
    just updating with all the files so i can link when write up

---
+++ 00_scripts/flye.loggetter.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "beepboop, copying files, boopbeep"
+
+cp /scratch/scw2160/02_outputs/flye_asm/flye_asm_1Dt100g_part2subset/flye.log ./flye_assembly/1Dt100g_flye.log
+cp /scratch/scw2160/02_outputs/flye_asm/flye_asm_1Dt100h_part2subset/flye.log ./flye_assembly/1Dt100h_flye.log
+cp /scratch/scw2160/02_outputs/flye_asm/flye_asm_1Dt1h_part2/flye.log ./flye_assembly/1Dt1h_flye.log
+cp /scratch/scw2160/02_outputs/flye_asm/flye_asm_1Dt2d_part2/flye.log ./flye_assembly/1Dt2d_flye.log
+cp /scratch/scw2160/02_outputs/flye_asm/flye_asm_2Dt1e_part2/flye.log ./flye_assembly/2Dt1e_flye.log
+cp /scratch/scw2160/02_outputs/flye_asm/flye_asm_2Dt1l_part2/flye.log ./flye_assembly/2Dt1l_flye.log
+cp /scratch/scw2160/02_outputs/flye_asm/flye_asm_3Dt1c_part2/flye.log ./flye_assembly/3Dt1c_flye.log
+cp /scratch/scw2160/02_outputs/flye_asm/flye_asm_3Dt2c_part2/flye.log ./flye_assembly/3Dt2c_flye.log
+cp /scratch/scw2160/02_outputs/flye_asm/flye_asm_3Dt2h_part2/flye.log ./flye_assembly/3Dt2h_flye.log
+cp /scratch/scw2160/02_outputs/flye_asm/flye_asm_3Dt2j_part2subset/flye.log ./flye_assembly/3Dt2j_flye.log
+echo "beepboop finished boopbeep"
---
+++ 00_scripts/flyeloganalysis.R
@@ -0,0 +1,50 @@
+library(dplyr)
+library(tidyverse)
+library(gt)
+#------------------------------create list / name stuff-------------------------------
+flyelog <- list.files("02_middle-analysis_outputs/flyestuff", pattern = ".log", full.names = TRUE)
+names <- list.files("02_middle-analysis_outputs/flyestuff", pattern = ".log", full.names = FALSE)
+names <- sub("_.*", "", names)
+#-----------------------------import data / create-------------------------------
+final <- data.frame(accession=character(), key=character(), value=numeric())
+
+for (i in 1:length(names)) {
+  #i <- 3
+  log <- tail(read.fwf(flyelog[i], widths = 100), 25)
+  cut <- as.data.frame(log[-grep('\\[', log$V1),]) 
+  names(cut) <- c("data")
+  cut <- as.data.frame(cut[!(is.na(cut$data) | cut$data==""), ])
+  names(cut) <- c("data")
+  key <- cut[grep(':', cut$data),]
+  value <- cut[-grep(':', cut$data),]
+  frame <- data.frame(key, value)
+  frame$accession <- names[i]
+  frame$key <- gsub(":", "", as.character(frame$key))
+  final <- rbind(final, frame)
+}
+
+final <- pivot_wider(final, names_from = key, values_from = value )
+#-----------------------------tabulate--------------------------------
+tablenames <- c("Total length", "Fragments", "Fragments N50", "Largest frg", "Scaffolds", "Mean coverage")
+final[tablenames] <- sapply(final[tablenames], as.numeric)
+final %>%
+  arrange(desc(`Mean coverage`)) %>%
+  gt() %>%
+  fmt_number(
+    columns = tablenames,
+    use_seps = TRUE,
+    decimals = 0)  %>%
+  tab_source_note(source_note = "Source: flye.log files produced in flye_asm analysis, August 27th 2024") %>%
+  opt_row_striping() %>%
+  opt_stylize(style = 5, color = "blue") %>%
+  data_color(
+    columns = `Mean coverage`,
+    method = "numeric",
+    palette = "Purples",
+    domain = c(150, 450),
+    reverse = FALSE
+  ) %>%
+  tab_header(
+    title = "Table 3 - Data obtained from flye.log outputs",
+    subtitle = md("of samples taken from the skin microbiome of *Dendrobates tinctorius*")
+  )
