b4cfb071f3294f9b787b2b2cf64e759fb5b892e2
Nancy <nancy@aptemus.com>
2024-12-30 17:54:48 +0000

    Update to tree_qa script
    Removed dots from column names, and extracted accession without using lapply(), which then gives trouble when trying to save the file to csv.

--- 00_scripts/CheckMtreeqa.R
+++ 00_scripts/CheckMtreeqa.R
@@ -2,15 +2,18 @@ library(dplyr)
 library(tidyverse)
 library(gt)
 #------------------------------import-------------------------------
-treeqa<- read.delim("02_middle-analysis_outputs/CheckM/checkmtreeqa/checkmtreeqa_bigsummary.txt", header = TRUE)
+treeqa<- read.delim("02_middle-analysis_outputs/CheckM/checkmtreeqa/checkmtreeqa_bigsummary.txt", header = TRUE, sep = "\t")
 #-----------------------------clean-------------------------------
-treeqa$Bin.Id <- as.vector(lapply(treeqa$Bin.Id, function(x) gsub("flye_asm_|_part2", "", x)))
+treeqa$Bin.Id <- gsub("flye_asm_|_part2", "", as.character(treeqa$Bin.Id))
+names(treeqa) <- gsub("\\.", "", names(treeqa))
 #-----------------------------tabulate--------------------------------
-str(treeqa)
 write.csv(treeqa, file = "03_final_outputs/metadata_and_quality_tables/checkmtreeqa.csv", row.names = FALSE)
-
 #----------------------------stuff for Markdown doc [DONT RUN]-------------------
-columnnames <- c("Bin.Id", "X..unique.markers..of.43.", "Taxonomy..contained.","Taxonomy..sister.lineage.")
+
+
+columnnames <- c("BinId", "Xuniquemarkersof43", "Taxonomycontained","Taxonomysisterlineage")
+treeqa$Taxonomycontained <- gsub(";", "&#8594;", treeqa$Taxonomycontained)
+treeqa$Taxonomysisterlineage <- gsub(";", "&#8594;", treeqa$Taxonomysisterlineage)
 treeqa[columnnames] %>%
   gt() %>%
   tab_source_note(source_note = "Source: CheckM tree_qa performed on 2024-12-30") %>%
@@ -19,4 +22,11 @@ treeqa[columnnames] %>%
   tab_header(
     title = "Table 6 - subset of data obtained from secondary CheckM analysis",
     subtitle = md("of samples taken from the skin microbiome of *Dendrobates tinctorius*, including taxonomy at various levels")
+  ) %>%
+  fmt_markdown(
+    columns = c(Taxonomycontained, Taxonomysisterlineage),
+    rows = everything(),
+    md_engine = "markdown"
   )
+
+
