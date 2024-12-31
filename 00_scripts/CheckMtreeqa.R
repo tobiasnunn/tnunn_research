20d1f52225ae83ba449e471cd260ce62f92c4ed9
tobiasnunn <59334337+tobiasnunn@users.noreply.github.com>
2024-12-31 11:56:05 +0000

    notebook
    part way through notebooking for treeqa, need to push so i can link to correct version of treeqa.R

--- 00_scripts/CheckMtreeqa.R
+++ 00_scripts/CheckMtreeqa.R
@@ -8,25 +8,3 @@ treeqa$Bin.Id <- gsub("flye_asm_|_part2", "", as.character(treeqa$Bin.Id))
 names(treeqa) <- gsub("\\.", "", names(treeqa))
 #-----------------------------tabulate--------------------------------
 write.csv(treeqa, file = "03_final_outputs/metadata_and_quality_tables/checkmtreeqa.csv", row.names = FALSE)
-#----------------------------stuff for Markdown doc [DONT RUN]-------------------
-
-
-columnnames <- c("BinId", "Xuniquemarkersof43", "Taxonomycontained","Taxonomysisterlineage")
-treeqa$Taxonomycontained <- gsub(";", "&#8594;", treeqa$Taxonomycontained)
-treeqa$Taxonomysisterlineage <- gsub(";", "&#8594;", treeqa$Taxonomysisterlineage)
-treeqa[columnnames] %>%
-  gt() %>%
-  tab_source_note(source_note = "Source: CheckM tree_qa performed on 2024-12-30") %>%
-  opt_row_striping() %>%
-  opt_stylize(style = 5, color = "blue") %>%
-  tab_header(
-    title = "Table 6 - subset of data obtained from secondary CheckM analysis",
-    subtitle = md("of samples taken from the skin microbiome of *Dendrobates tinctorius*, including taxonomy at various levels")
-  ) %>%
-  fmt_markdown(
-    columns = c(Taxonomycontained, Taxonomysisterlineage),
-    rows = everything(),
-    md_engine = "markdown"
-  )
-
-
