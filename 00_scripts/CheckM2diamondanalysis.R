17f417db32ff18549bcc2d987c0c994aaba45b67
tobiasnunn <59334337+tobiasnunn@users.noreply.github.com>
2025-01-03 16:17:38 +0000

    bunch of stuff, not finished
    basically all the stuff i did today, it is very spurradic and i have lost the plot a bit, but the outputs from this could be powerful

--- 00_scripts/CheckM2diamondanalysis.R
+++ 00_scripts/CheckM2diamondanalysis.R
@@ -5,6 +5,14 @@ library(gt)
 library(readxl)
 library(tidyjson)
 library(purrr)
+# statistical packages
+# prob not all used, but i am not sure what does what, so i ported all of them
+library(ggplot2)
+library(psych)
+library(RVAideMemoire)
+library(stats)
+library(tidyverse)
+library(cowplot)
 #-----------------------------------import---------------------------------
 
 # DIAMOND RESULTS
@@ -112,7 +120,110 @@ names(cumcomb) <- c("accession.e", "ko_value.e", "tot_rows.e", "accession.d", "k
 
 write.csv(cumcomb, "02_middle-analysis_outputs/CheckM2_20241228/cumcomb.csv", row.names = FALSE)
 
-# write up investigation into DIAMOND_RESULTS.tsv and output a table with some key metrics
+#----------------------------------statistical analysis----------------------
+# let's start by doing a comparison of the unique percentages, then a wholesale difference analysis
+shapiro.test(unique$diamond_per)
+shapiro.test(unique$egg_per)
+# p1 = 0.6 / p2 = 0.06 - not sig dif from normal (parametric)
+# diff btwn groups -> Parametric -> 1 ind var -> two categories -> unpaired -> Variances homogeneous?
+
+# F-test for variance
+
+res <- var.test(unique$diamond_per, unique$egg_per)
+res
+?var.test
+# right, that is a lot, p=0.27
+ser <- var.test(unique$egg_per, unique$diamond_per)
+ser
+# there are differences, im going to assume the first one is right
+# what does it mean when p>0.05? why can noone give me a good answer for this?
+# ok, im just going to take a guess, if it is like the shapiro then a p>0.05
+# meaning they are not significantly different, thus the variances are homogeneous
+# in the end i dont think it matters, theres only one function for t.test, oh well
+
+# Student's t-test
+
+ttest <- t.test(unique$diamond_per, unique$egg_per)
+# p = 0.14 so they are ...
+#he Independent-Samples t Test The null hypothesis is that the means of the two populations are the same: µ1 = µ2
+# so we accept the null because p> 0.05 so they are statistically the same, i think
+
+#--------------------------------testing non-unique---------------------------
+# due to the nature of the pipeline, i am going to need to do some stuff to
+# prepare the data for transformation
+cum_egg <- eggnog_kos[c("accession", "ko_value")]
+cum_diamond <- dires[c("accession", "ko_value")]
+cumbined <- rbind(cum_egg, cum_diamond)
+#combined <- unique(combined[c("accession", "ko_value")])
+cumbined <- cumbined[!is.na(cumbined$accession), ]
+# creating a combined accession and ko column to make it easier to match to combined
+cum_egg$accko <- paste(cum_egg$accession, cum_egg$ko_value, sep = "-")
+cum_diamond$accko <- paste(cum_diamond$accession, cum_diamond$ko_value, sep = "-")
+cumbined$accko <- paste(cumbined$accession, cumbined$ko_value, sep = "-")
+
+cumbined$egg <- cumbined$accko %in% cum_egg$accko
+cumbined$diamond <- cumbined$accko %in% cum_diamond$accko
+
+cumulative <- cumbined %>% 
+  group_by(accession) %>% 
+  summarise(
+    tot_rows = n(), 
+    diamond = length(accession[diamond == TRUE]), 
+    egg = length(accession[egg == TRUE]), 
+    diamond_per = diamond / tot_rows,
+    egg_per = egg / tot_rows)
+
+# testing
+shapiro.test(cumulative$diamond_per)
+shapiro.test(cumulative$egg_per)
+
+cumttest <- t.test(cumulative$diamond_per, cumulative$egg_per)
+cumttest
+# hell, p < 0.05 so they are different
+
+#---------------------------plots----------------------------------------
+#whenever i did this before, they had me do it with a graph, so why not do one here
+unibox <- unique %>% 
+  select(diamond_per, egg_per) %>% 
+  pivot_longer(everything(), names_to = "variable", values_to = "value") %>%
+  ggplot(aes(x=variable,y=value)) +
+  geom_boxplot(fill = c("purple","yellow")) +geom_jitter(alpha=0.3, width = 0.1) + ylim(0.85,1)
+
+cumbox <- cumulative %>% 
+  select(diamond_per, egg_per) %>% 
+  pivot_longer(everything(), names_to = "variable", values_to = "value") %>%
+  ggplot(aes(x=variable,y=value)) +
+  geom_boxplot(fill = c("purple","yellow")) +geom_jitter(alpha=0.3, width = 0.1) + ylim(0.85,1)
+
+plot_grid(unibox, cumbox, labels=c("Unique", "Cumulative"), ncol = 2, nrow = 1)
+
+#-------------------------Direct comp of diamond and eggnog--------------
+# previous comparisons took place between one of the methods and a 
+# "total" list containing KOs from both, but, having built up the code for this
+# can i now compare them to each other directly?
+
+length(unique_diamond$accko[unique_diamond$accko %in% unique_egg$accko])
+length(unique_diamond$accko)
+length(unique_egg$accko)
+
+length(unique_diamond$accko[unique_diamond$accko %in% unique_egg$accko]) / length(unique_diamond$accko)
+length(unique_diamond$accko[unique_diamond$accko %in% unique_egg$accko]) / length(unique_egg$accko)
+
+
+#test
+frog <- c(1,1,1,1,35,69)
+toad <- c(1,1,1,1,1,2,5,43)
+pond <- frog[frog %in% toad]
+dnop <- toad[toad %in% frog]
+# dopn <- frog[toad %in% frog]
+write.csv(cum_egg, file = "cum_egg.csv")
+
+replicate(3, cum_egg, simplify = FALSE) %>%
+  imap_dfr(~ cum_egg$accko %>% 
+             mutate(id = cum_egg$accko))
+
+#9oooooooooooooooooooooooooooooooooo54rrrtrgfffff
+# write u# write u# write up investigation into DIAMOND_RESULTS.tsv and output a table with some key metrics
 # like how many are in both and how many are in only one, maybe scale up to all accessions. 
 # prelim to know what our options are when heatmapping
 # remember to talk about the excel stuff
