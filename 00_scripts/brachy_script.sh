705dfdf44598b640d4d9ee2ff1b8a1e7ab573a0a
tobiasnunn <59334337+tobiasnunn@users.noreply.github.com>
2025-01-19 10:47:45 +0000

    downloaded test eggnog stuff
    did it yesterday, forgot, my bad

---
+++ 00_scripts/brachy_script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --account=scw2160
+#SBATCH --job-name=eggnogmapper
+#SBATCH --partition=compute           # compute (max resources: 192g; ), highmem (max resources: 384g; 40 threads; 72h)
+#SBATCH --ntasks=5                   # Number of tasks
+#SBATCH --time=12:00:00                # Run time
+#SBATCH --mem=50g                     # Memory pool for all cores (in MB: 4096 == 4 GB)
+#SBATCH -o /scratch/scw2160/09_logs/eggnogmapper-%A.out
+#SBATCH -e /scratch/scw2160/09_logs/eggnogmapper-%A.err
+
+
+eval "$(/apps/languages/anaconda/2024.02/bin/conda shell.bash hook)"
+source activate
+module load eggnog-mapper/2.1.12
+
+mkdir GCA_019115335.1_new
+mkdir GCA_019118275.1_new
+mkdir GCF_000225825.1_new
+mkdir GCF_005490785.1_new
+mkdir GCF_015209585.1_new
+echo "process start" > GCA_019115335.1_start.txt
+emapper.py -m diamond --itype genome --genepred prodigal -i /home/b.tbn23vlc/fasta_files/GCA_019115335.1_ASM1911533v1_genomic.fna -o GCA_019115335.1 --output_dir /home/b.tbn23vlc/GCA_019115335.1_new/ --cpu 0 --dmnd_ignore_warnings --evalue 0.001 --score 60 --pident 40 --query_cover 20 --subject_cover 20 --tax_scope auto --target_orthologs all --go_evidence non-electronic --pfam_realign none --report_orthologs --decorate_gff yes --excel --dbmem
+emapper.py -m diamond --itype genome --genepred prodigal -i /home/b.tbn23vlc/fasta_files/GCA_019118275.1_ASM1911827v1_genomic.fna -o GCA_019118275.1 --output_dir /home/b.tbn23vlc/GCA_019118275.1_new/ --cpu 0 --dmnd_ignore_warnings --evalue 0.001 --score 60 --pident 40 --query_cover 20 --subject_cover 20 --tax_scope auto --target_orthologs all --go_evidence non-electronic --pfam_realign none --report_orthologs --decorate_gff yes --excel --dbmem
+emapper.py -m diamond --itype genome --genepred prodigal -i /home/b.tbn23vlc/fasta_files/GCF_000225825.1_ASM22582v2_genomic.fna -o GCF_000225825.1 --output_dir /home/b.tbn23vlc/GCF_000225825.1_new/ --cpu 0 --dmnd_ignore_warnings --evalue 0.001 --score 60 --pident 40 --query_cover 20 --subject_cover 20 --tax_scope auto --target_orthologs all --go_evidence non-electronic --pfam_realign none --report_orthologs --decorate_gff yes --excel --dbmem
+emapper.py -m diamond --itype genome --genepred prodigal -i /home/b.tbn23vlc/fasta_files/GCF_005490785.1_ASM549078v1_genomic.fna -o GCF_005490785.1 --output_dir /home/b.tbn23vlc/GCF_005490785.1_new/ --cpu 0 --dmnd_ignore_warnings --evalue 0.001 --score 60 --pident 40 --query_cover 20 --subject_cover 20 --tax_scope auto --target_orthologs all --go_evidence non-electronic --pfam_realign none --report_orthologs --decorate_gff yes --excel --dbmem
+emapper.py -m diamond --itype genome --genepred prodigal -i /home/b.tbn23vlc/fasta_files/GCF_015209585.1_ASM1520958v1_genomic.fna -o GCF_015209585.1 --output_dir /home/b.tbn23vlc/GCF_015209585.1_new/ --cpu 0 --dmnd_ignore_warnings --evalue 0.001 --score 60 --pident 40 --query_cover 20 --subject_cover 20 --tax_scope auto --target_orthologs all --go_evidence non-electronic --pfam_realign none --report_orthologs --decorate_gff yes --excel --dbmem
+echo "process end" > GCF_015209585.1_end.txt
