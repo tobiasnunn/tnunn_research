c23c334f1fc9acfc10b05f66a0474d6ce3600d3a
tobiasnunn <59334337+tobiasnunn@users.noreply.github.com>
2025-01-27 08:48:00 +0000

    Ran API
    did code, got fastas, is good

--- 00_scripts/NCBIfastacollectorprototype.R
+++ 00_scripts/NCBIfastacollectorprototype.R
@@ -14,16 +14,25 @@ local <- filter(combi, sample_type == "bangor" & family != "f__")
 
 countbygenus <- local %>% group_by(genus) %>% count()
 
-final <- rbind(slice_sample(filter(subset, genus == "g__Microbacterium"), n=8), slice_sample(filter(subset, genus == "g__Sphingomonas"), n=6))
-final <- rbind(final, slice_sample(filter(subset, genus == "g__Pantoea"), n=9))
-final <- rbind(final, slice_sample(filter(subset, genus == "g__Brevibacterium"), n=9))
-final <- rbind(final, slice_sample(filter(subset, genus == "g__Brachybacterium"), n=9))
-
 # NOTE: improve the automation on this bit, a for loop could do that
 fastalist <- filter(subset, 1 == 0)
 
 for (i in 1:nrow(countbygenus)) {
   fastalist <- rbind(fastalist, slice_sample(filter(subset, genus == countbygenus[i,"genus"]), n=10-countbygenus$n[i]))
+}
+
+
+# call the API, get .fastas
+urlstring <- 'https://api.ncbi.nlm.nih.gov/datasets/v2/'
+api_header <- 'd4f921c91015c68f48f656a99ad5bcf84a08'
+
+for (i in 1:nrow(fastalist)) {
+  accession <- fastalist$accession[i]
+  req <- request(base_url = urlstring) %>% 
+    req_auth_bearer_token(api_header) %>%
+    req_url_path_append("genome", "accession", accession, "download") %>%
+    req_url_query( include_annotation_type ='GENOME_FASTA') %>%
+    req_perform(path = paste0("02_middle-analysis_outputs/ncbi_stuff/fasta/", accession, '.zip'))
 }
 
 
