89bf0cdd5086df2c75bef487b98ff9c530e4ef12
tobiasnunn <59334337+tobiasnunn@users.noreply.github.com>
2025-01-28 21:21:49 +0000

    KEGG pipeline stuff
    partway there, just need to wait for all 50 to be done and business will be booming

---
+++ 00_scripts/KEGG_pipeline_pt1.R
@@ -0,0 +1,13 @@
+library(dplyr)
+library(tidyr)
+library(tidyverse)
+library(readxl)
+
+# read in metadata file
+metadata <- read_delim("02_middle-analysis_outputs/eggnog_stuff/post_eggnog_pipeline/prototype_metadata.tsv", delim = "\t")
+
+# read in all the .xlsx files
+for (i in nrow(metadata)) {
+  intermed <- read_excel(paste0("02_middle-analysis_outputs/eggnog_stuff/eggnog_outputs/", metadata$accession[i], ".emapper.annotations.xlsx"),
+                         skip = 2, col_names = TRUE)
+}
---
+++ 00_scripts/metadata_maker.R
@@ -0,0 +1,16 @@
+library(dplyr)
+library(tidyr)
+library(tidyverse)
+# read in the chosen accessions for prototype
+all_genera <- read.delim("02_middle-analysis_outputs/analysis_tables/genera_analysis_combined.tsv")
+just_prototype <- read.delim("02_middle-analysis_outputs/analysis_tables/prototypelist.tsv")
+# filter 1 by 2 so just genera in prototype list or a bangor sample
+all_prototype <- all_genera %>% filter(all_genera$accession %in% just_prototype$accession | all_genera$sample_type == "bangor") %>%
+  filter(.$accession != "1Dt100h")
+
+# ok, now i make the metadata file, this has two columns (mag_id = accession / bac_taxon = genus)
+
+metadata <- all_prototype %>% select(c("accession", "genus")) %>%
+  mutate(across(c('genus'), substr, 4, nchar(genus)))
+
+write_delim(metadata, "02_middle-analysis_outputs/eggnog_stuff/post_eggnog_pipeline/prototype_metadata.tsv", delim = "\t")
