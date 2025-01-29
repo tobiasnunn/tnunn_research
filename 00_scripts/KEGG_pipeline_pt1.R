e2d44f2d250c4765e57c7e3e245e9e57f74bc2fb
tobiasnunn <59334337+tobiasnunn@users.noreply.github.com>
2025-01-29 08:44:22 +0000

    hawk pulldowns and notebook
    all but one of the eggnogs were good, brought down but still not done yet, need to think about error handling

--- 00_scripts/KEGG_pipeline_pt1.R
+++ 00_scripts/KEGG_pipeline_pt1.R
@@ -4,10 +4,10 @@ library(tidyverse)
 library(readxl)
 
 # read in metadata file
-metadata <- read_delim("02_middle-analysis_outputs/eggnog_stuff/post_eggnog_pipeline/prototype_metadata.tsv", delim = "\t")
+metadata <- read_delim("02_middle-analysis_outputs/eggnog_stuff/post_eggnog_pipeline/prototype_metadata.tsv", delim = "\t", show_col_types = FALSE)
 
 # read in all the .xlsx files
-for (i in nrow(metadata)) {
+for (i in 1:nrow(metadata)) {
   intermed <- read_excel(paste0("02_middle-analysis_outputs/eggnog_stuff/eggnog_outputs/", metadata$accession[i], ".emapper.annotations.xlsx"),
                          skip = 2, col_names = TRUE)
 }
