2baca5d0dca2e5075d0b555e55c20e80805f013b
tobiasnunn <59334337+tobiasnunn@users.noreply.github.com>
2025-04-05 12:43:53 +0100

    buncha crab
    i know abt alt + o and ctrl + shift + R now

--- BSX2030/crab_proj_2_figure2.R
+++ BSX2030/crab_proj_2_figure2.R
@@ -1,4 +1,6 @@
-# libraries
+# libraries and setup-----------------------------------------------------
+
+
 library(dplyr)
 library(tidyr)
 library(flextable)
@@ -10,7 +12,7 @@ library(ggplot2)
 library(gt)
 library(cowplot)
 
-# setup block
+
 rawdaynight <- (readxl::read_xlsx("lightvsdark.xlsx", sheet = 1) %>% 
                   select( -c(`...8`)) %>% 
                   pivot_longer(!`Crab No.`, names_to = "attribute", values_to = "reading")) %>% 
@@ -33,15 +35,18 @@ rawnightday <- (readxl::read_xlsx("lightvsdark.xlsx", sheet = 2) %>%
 
 combinedcycles <- rbind(rawdaynight, rawnightday)
 
-# normal graph 
+rm(rawdaynight)
+rm(rawnightday)
+
+#--------------------------red graph-----------------------------
 
 #x <- filter(combinedcycles, combinedcycles$cycle == "normal")
 # right, i figured out how to filter it to just the ones on the normal cycle, but i think i need to do the means thing again, because otherwise there are going to be like 100 lines on the thing.
-normal_means <- filter(combinedcycles, pigment == "Red") %>%
+red_means <- filter(combinedcycles, pigment == "Red") %>%
   group_by(cycle, time) %>%
   summarise(mean_reading = mean(reading, na.rm=TRUE))
 
-normal_graph <- ggplot(normal_means, aes(time, mean_reading, 
+red_graph <- ggplot(red_means, aes(time, mean_reading, 
                                          color = factor(cycle), 
                                          fill = factor(cycle),
                                          linetype = factor(cycle))) +
@@ -54,7 +59,7 @@ normal_graph <- ggplot(normal_means, aes(time, mean_reading,
   scale_colour_manual(values = c("firebrick1", "firebrick1")) +
   scale_fill_manual(values = c("red3", "red3")) +
   scale_linetype_manual(values = c("inverse" = "dotted", "normal" = "solid")) +
-  scale_x_continuous(breaks=unique(normal_means$time), minor_breaks = NULL) +
+  scale_x_continuous(breaks=unique(red_means$time), minor_breaks = NULL) +
   # claude-made
   scale_y_continuous(
     limits = c(1, 5),   # Use limits here instead of ylim()
@@ -84,19 +89,20 @@ normal_graph <- ggplot(normal_means, aes(time, mean_reading,
     curvature = -.3, arrow = arrow(length = unit(2, "mm"))
   ) + 
   geom_label(aes(x = 47, y = 4, label = "RPCH injection"), fill = "#D99594", colour = "black")
-normal_graph
+red_graph
 
-# inverse graph
+rm(red_means)
+#--------------------------black graph---------------------------------------
 # i really haveant the faintest why this is being so difficult
 # she left fucking NAs in the dataset, only 4 of them and only in inverted, wtf
 # so i remedied it with the na.rm=TRUE bit, jesus, that was painful
-inverted_means <- filter(combinedcycles, pigment == "Black") %>%
+black_means <- filter(combinedcycles, pigment == "Black") %>%
   group_by(cycle, time) %>%
   summarise(mean_reading = mean(reading, na.rm=TRUE))
 
 
 
-inverse_graph <- ggplot(inverted_means, aes(time, mean_reading, 
+black_graph <- ggplot(black_means, aes(time, mean_reading, 
                                             color = factor(cycle), 
                                             fill = factor(cycle),
                                             linetype = factor(cycle))) +
@@ -109,7 +115,7 @@ inverse_graph <- ggplot(inverted_means, aes(time, mean_reading,
   scale_colour_manual(values = c("grey27", "grey27")) +
   scale_fill_manual(values = c("black", "black")) +
   scale_linetype_manual(values = c("inverse" = "dotted", "normal" = "solid")) +
-  scale_x_continuous(breaks=unique(normal_means$time), minor_breaks = NULL) +
+  scale_x_continuous(breaks=unique(black_means$time), minor_breaks = NULL) +
   # claude-made
   scale_y_continuous(
     limits = c(1, 5),   # Use limits here instead of ylim()
@@ -139,64 +145,144 @@ inverse_graph <- ggplot(inverted_means, aes(time, mean_reading,
     curvature = -.3, arrow = arrow(length = unit(2, "mm"))
   ) + 
   geom_label(aes(x = 47, y = 4.8, label = "RPCH injection"), fill = "#D99594", colour = "black")
-inverse_graph
+black_graph
 
-# combining
-combi <- plot_grid(normal_graph, inverse_graph, labels = c('A', 'B'), label_size = 12)
+rm(black_means)
+
+## cowplot-ing-------------------------------------------------------------
+
+
+combi <- plot_grid(red_graph, black_graph, labels = c('A', 'B'), label_size = 12)
 combi
 ggsave("secondgraph.png", plot = combi, units = "cm", height = 10, width = 20)
 
-#stats tables
-##for loops
-#table making
-# normal
-p_values_normal <- data.frame(pigment=c("Red", "Red", "Red","Red","Red", 
-                                        "Black","Black","Black","Black","Black"), 
-                              start_time=c(0,30,50,70,75),
-                              end_time=c(30,50,70,75,100),
-                              p_value=c(NA),
-                              significant=c(NA))
-# loop, store result
-for (i in 1:nrow(p_values_normal)) {
+rm(black_graph)
+rm(red_graph)
+rm(combi)
+
+#------------------------------------stats tables------------------------------------
+
+
+## p_values table creation-------------------------------------------------
+
+
+pigment <- data.frame(pigment = c("Red", "Black"))
+cycle <- data.frame(cycle = c("normal", "inverse"))
+
+p_values <- data.frame(
+  start_time=c(0,30,50,70,75,30,70),
+  end_time=c(30,50,70,75,100,70,100),
+  p_value=c(NA),
+  significant=c(NA)) %>% 
+  cross_join(cycle)  %>%
+  cross_join(pigment) %>% 
+  arrange(pigment, cycle, start_time, end_time) %>% 
+  select(pigment, cycle, start_time, end_time, p_value, significant)
+
+## for loop with wilcoxon--------------------------------------------------
+
+
+for (i in 1:nrow(p_values)) {
   #i <- 1
-  x <- filter(rawdaynight, time==p_values_normal$start_time[i] & pigment==p_values_normal$pigment[i])
-  y <- filter(rawdaynight, time==p_values_normal$end_time[i] & pigment==p_values_normal$pigment[i])
-  z <- wilcox.test(x$reading, y$reading, paired = TRUE, correct = FALSE, alternative = "t")
-  p_values_normal$p_value[i] <- z$p.value
+  x <- filter(combinedcycles,
+              time==p_values$start_time[i] & 
+                cycle==p_values$cycle[i] & 
+                pigment==p_values$pigment[i])
+  
+  y <- filter(combinedcycles, 
+              time==p_values$end_time[i] & 
+                cycle==p_values$cycle[i] & 
+                pigment==p_values$pigment[i])
+  
+  z <- wilcox.test(x$reading, 
+                   y$reading, 
+                   paired = TRUE, 
+                   correct = FALSE, 
+                   alternative = "t")
+  
+  p_values$p_value[i] <- z$p.value
   if (z$p.value < 0.05) {
-    p_values_normal$significant[i] <- c("*")
+    p_values$significant[i] <- "*"
   }
 }
 
-# inverse
-p_values_inverse <- data.frame(pigment=c("Red", "Red", "Red","Red","Red", 
-                                        "Black","Black","Black","Black","Black"), 
-                              start_time=c(0,30,50,70,75),
-                              end_time=c(30,50,70,75,100),
-                              p_value=c(NA),
-                              significant=c(NA))
-# loop, store result
-for (i in 1:nrow(p_values_inverse)) {
-  #i <- 1
-  x <- filter(rawnightday, time==p_values_inverse$start_time[i] & pigment==p_values_inverse$pigment[i])
-  y <- filter(rawnightday, time==p_values_inverse$end_time[i] & pigment==p_values_inverse$pigment[i])
-  z <- wilcox.test(x$reading, y$reading, paired = TRUE, correct = FALSE, alternative = "t")
-  p_values_inverse$p_value[i] <- z$p.value
-  if (z$p.value < 0.05) {
-    p_values_inverse$significant[i] <- c("*")
-  }
+
+## table design ------------------------------------------------------------
+
+
+### data transformation-----------------------------------------------------
+
+
+p_ivot_values <- select(p_values, -significant) %>% 
+  unite("time_diff", start_time:end_time, sep = " - ") %>% 
+  pivot_wider(names_from = time_diff, values_from = p_value)
+
+#### scientific notation -----------------------------------------------------
+
+ 
+# needed claude for this bit, very complecated thing to do, need to get better
+# so i can do this alone
+
+# Get the names of all numeric columns
+numeric_cols <- names(p_ivot_values)[sapply(p_ivot_values, is.numeric)]
+
+# Create a named list for formatting
+format_list <- setNames(
+  replicate(length(numeric_cols), 
+            function(x) formatC(x, format = "e", digits = 3), 
+            simplify = FALSE),
+  numeric_cols
+)
+
+# also needed help to colour cells grey, heres the funct for that
+# Create a function that will color cells based on their values
+color_if_greater <- function(x) {
+  ifelse(as.numeric(x) > 0.05, "grey70", "black")
 }
+### table styling---------------------------------------------------------------
+
+
+p_table <- flextable(p_ivot_values) %>% 
+  set_header_labels(pigment = "Pigment",
+                    cycle = "Cycle") %>% 
+  theme_vanilla() %>% 
+  add_footer_lines("Data collected on 2025-03-10 at Deiniol Road, Brambell Building, 1st Floor Lab B1") %>% 
+  color(part = "footer", color = "#666666") %>% 
+  set_caption(caption = "P values for Wilcoxon Paired Rank Test") %>% 
+  add_header_row(colwidths = c(2, 7),
+                 values = c("", "P values for start time - end time (mins from primary injection)")) %>% 
+  align(i = 1, j = 3, 
+        align = "center", 
+        part = c("header")) %>%  
+  set_table_properties(width = 1, 
+                       layout = "autofit") %>% 
+  set_formatter(values = format_list) %>%
+  footnote(i = 1, j = 2,
+           part = "header",
+           ref_symbols = "*",
+           value = as_paragraph("P values taken between pairs of times when measurements were taken for a Wilcoxon Paired Rank Test")) %>% 
+  # Apply to all numeric columns
+  color(
+    color = color_if_greater,
+    part = "body",
+    j = numeric_cols
+  )
+p_table
+# also needed clauds help for the grey text
+
+# a lesson on NAs -----------------------------------------------
+
 
 # how i figured out the problem with the inverse values, it was the random-ass NAs she didnt remove
 # despite the fact thats what she said she did
-# normal_means <- filter(combinedcycles, combinedcycles$cycle == "normal") %>%
+# red_means <- filter(combinedcycles, combinedcycles$cycle == "normal") %>%
 #   group_by(pigment, time) %>%
 #   summarise(mean_reading = mean(reading))
 # 
-# inverse_means <- filter(combinedcycles, combinedcycles$cycle == "inverse") %>% 
+# black_means <- filter(combinedcycles, combinedcycles$cycle == "inverse") %>% 
 #   group_by(pigment, time) %>% 
 #   mutate(mean_reading = mean(reading))
 # 
-# inverted_means <- filter(combinedcycles, combinedcycles$cycle == "inverse") %>% 
+# black_means <- filter(combinedcycles, combinedcycles$cycle == "inverse") %>% 
 #   group_by(pigment, time) %>%
 #   summarise(mean_reading = mean(reading, na.rm = TRUE))
